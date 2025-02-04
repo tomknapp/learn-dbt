35ae1c5ab53978596a499e0adc2ca1c6e2146f57
Tom Knapp <tom.knapp@hippodigital.co.uk>
2025-02-04 14:20:41 +0000

    Define sources and refactor models to use jinja style sources and expand ctes

--- dbt_playground/models/playground/customer_geography.sql
+++ dbt_playground/models/playground/customer_geography.sql
@@ -1,17 +1,26 @@
-Select
-    id,
-    customer_id,
-    postcode,
-    date_modified
-from
-    public.address
-where
-    (customer_id, date_modified) in (
-        select
-            customer_id,
-            max(date_modified)
-        from
-            public.address
-        group by
-            customer_id
-    )
+with source_address as (
+    Select * from {{ source("postgres_example_db", "address") }}
+),
+
+final as (
+
+    Select
+        id,
+        customer_id,
+        postcode,
+        date_modified
+    from
+        source_address
+    where
+        (customer_id, date_modified) in (
+            select
+                customer_id,
+                max(date_modified)
+            from
+                source_address
+            group by
+                customer_id
+        )
+)
+
+Select * from final
--- dbt_playground/models/playground/customers.sql
+++ dbt_playground/models/playground/customers.sql
@@ -2,17 +2,26 @@
 A flattened source of customers
 */
 
-with geography as (
+with source_customers as (
+    Select * from {{ source("postgres_example_db", "customer") }}
+),
+
+geography as (
     Select * from {{ ref('customer_geography') }}
+),
+
+final as (
+
+    Select
+    source_customers.id,
+    source_customers.user_name,
+    source_customers.first_name,
+    source_customers.last_name,
+    source_customers.is_administrator,
+    geography.postcode
+    from source_customers
+    left outer join geography
+    on source_customers.id = geography.customer_id
 )
 
-select
-customer.id,
-customer.user_name,
-customer.first_name,
-customer.last_name,
-customer.is_administrator,
-geography.postcode
-from customer
-left outer join geography
-on customer.id = geography.customer_id
+Select * from final
